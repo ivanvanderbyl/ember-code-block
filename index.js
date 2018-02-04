5ee89dc600fab63d32a76cd091f7abe7edfbecca
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-02-04 11:47:26 -0600

    Started refactoring loader to be more efficient

--- index.js
+++ index.js
@@ -1,170 +1,203 @@
-/* eslint node: true */
+const path = require('path')
+const Funnel = require('broccoli-funnel')
+const mergeTrees = require('broccoli-merge-trees')
+const AMDDefineFilter = require('./lib/amd-define-filter')
+const rewriteLanguageDefinition = require('./lib/rewrite-language-definition')
+const rename = require('broccoli-stew').rename
+const fs = require('fs')
+const hljs = require('highlight.js')
+var inclusionFilter = require('./lib/inclusion-filter')
+var exclusionFilter = require('./lib/exclusion-filter')
 
-const path = require('path');
-const Funnel = require('broccoli-funnel');
-const mergeTrees = require('broccoli-merge-trees');
-const AMDDefineFilter = require('./lib/amd-define-filter');
-const rewriteLanguageDefinition = require('./lib/rewrite-language-definition');
-const rename = require('broccoli-stew').rename;
-const fs = require('fs');
-const hljs = require('highlight.js');
-var inclusionFilter = require('./lib/inclusion-filter');
-var exclusionFilter = require('./lib/exclusion-filter');
-
-const STRING_CAMELIZE_REGEXP_1 = (/(\-|\_|\.|\s)+(.)?/g);
-const STRING_CAMELIZE_REGEXP_2 = (/(^|\/)([A-Z])/g);
+const STRING_CAMELIZE_REGEXP_1 = /(\-|\_|\.|\s)+(.)?/g
+const STRING_CAMELIZE_REGEXP_2 = /(^|\/)([A-Z])/g
 
 function camelize(key) {
-  return key.replace(STRING_CAMELIZE_REGEXP_1, function(match, separator, chr) {
-    return chr ? chr.toUpperCase() : '';
-  }).replace(STRING_CAMELIZE_REGEXP_2, function(match, separator, chr) {
-    return match.toLowerCase();
-  });
+	return key
+		.replace(STRING_CAMELIZE_REGEXP_1, function(match, separator, chr) {
+			return chr ? chr.toUpperCase() : ''
+		})
+		.replace(STRING_CAMELIZE_REGEXP_2, function(match, separator, chr) {
+			return match.toLowerCase()
+		})
 }
 function normalizeLanguageName(name) {
-  if (/^\d+/.test(name)) {
-    name = "lang-"+name;
-  }
-  return camelize(name);
+	if (/^\d+/.test(name)) {
+		name = 'lang-' + name
+	}
+	return camelize(name)
 }
 
 function reorderForCompatibility(languages) {
-  let index = languages.findIndex(function(lang) {
-    return lang.name ===  'cpp';
-  });
-  let removed = languages.splice(index, 1);
-  languages.unshift(removed[0]);
-  return languages;
+	let index = languages.findIndex(function(lang) {
+		return lang.name === 'cpp'
+	})
+	let removed = languages.splice(index, 1)
+	languages.unshift(removed[0])
+	return languages
 }
 
 module.exports = {
-  name: 'ember-code-block',
+	name: 'ember-code-block',
 
-  included(app) {
-    this._super.included && this._super.included.apply(this, arguments);
-    this.app = app;
+	included(app) {
+		this._super.included && this._super.included.apply(this, arguments)
+		let target = findTargetHost(this, app)
+		this.app = app
 
-    let config = app.project.config(app.env) || {};
-    // let addonConfig = config[this.name] || {};
-    let addonConfig = {
-      style: 'tomorrow-night-eighties',
-      languages: {
-        // only: ['cos']
-      }
-    }
+		let config = app.project.config(app.env) || {}
+		// let addonConfig = config[this.name] || {};
+		let addonConfig = {
+			style: 'tomorrow-night-eighties',
+			languages: {
+				// only: ['cos']
+			},
+		}
 
-    this.languages = this.getLanguages(addonConfig);
-    this.style = addonConfig.style || "default";
+		this.languages = this.getLanguages(addonConfig)
+		this.style = addonConfig.style || 'default'
 
-    this.import(path.join('vendor', 'highlight', 'highlight.js'));
+		target.import(path.posix.join('vendor', 'highlight', 'highlight.js'), {
+			using: [{ transformation: 'amd', as: 'highlight' }],
+		})
 
-    let importAssert = this.import.bind(this);
-    this.languages.forEach((function(language) {
-      importAssert(path.join('vendor', 'highlight', `${language.file}`));
-    }))
+		// this.import(path.join('vendor', 'highlight', 'highlight.js'))
 
-    app.import('highlight/styles.css');
-  },
+		// let importAssert = this.import.bind(this)
+		// this.languages.forEach(function(language) {
+		// 	importAssert(path.join('vendor', 'highlight', `${language.file}`))
+		// })
 
-  getLanguages(config) {
-    let allLanguages = this._allLanguages();
-    let languages = config.languages || {only: [], except: []}
-    let onlyLanguages = languages.only || [];
-    let exceptLanguages = languages.except || [];
-    return allLanguages
-      .filter(inclusionFilter(onlyLanguages))
-      .filter(exclusionFilter(exceptLanguages));
-  },
+		// app.import('highlight/styles.css')
+	},
 
-  treeForStyles() {
-    let srcPath = path.join(require.resolve('highlight.js'), '..', '..', 'styles');
+	getLanguages(config) {
+		let allLanguages = this._allLanguages()
+		let languages = config.languages || { only: [], except: [] }
+		let onlyLanguages = languages.only || []
+		let exceptLanguages = languages.except || []
+		return allLanguages
+			.filter(inclusionFilter(onlyLanguages))
+			.filter(exclusionFilter(exceptLanguages))
+	},
 
-    try {
-      fs.statSync(path.join(srcPath, this.style)).isFile();
-    }catch(err) {
-      this.ui.writeWarnLine('[ember-code-block] style does not exist', this.style);
-      // return null;
-    }
+	treeForVendor() {
+		let trees = []
 
-    let tree = new Funnel(srcPath, {
-      include: [`${this.style}.css`],
-      annotation: `Funnel: highlight.js style`
-    });
+		// Load highlight.js into vendor tree as `highlight`
+		trees.push(this._highlightTree())
 
-    return rename(tree, function() {
-      return `/highlight/styles.css`;
-    });
-  },
+		// Load languages into vendor tree as `highlight/<language>`
+		let languageTreeFactory = this._languageTree
+		this.languages.forEach(function(language) {
+			trees.push(languageTreeFactory(language))
+		})
 
-  treeForVendor() {
-    let trees = [];
+		return mergeTrees(trees)
+	},
 
-    // Load highlight.js into vendor tree as `highlight`
-    trees.push(this._highlightTree())
+	_allLanguages() {
+		let languages = []
+		let languagesPath = path.join(require.resolve('highlight.js'), '../languages')
+		fs.readdirSync(languagesPath).map(function(file) {
+			let lang = require(path.join(languagesPath, file))(hljs)
 
-    // Load languages into vendor tree as `highlight/<language>`
-    let languageTreeFactory = this._languageTree;
-    this.languages.forEach(function(language) {
-      trees.push(languageTreeFactory(language))
-    });
+			let definition = {
+				aliases: lang.aliases || [],
+				variable: normalizeLanguageName(file.split('.')[0]),
+				name: file.split('.')[0],
+				file: file,
+			}
 
-    return mergeTrees(trees);
-  },
+			languages.push(definition)
+		})
 
-  _allLanguages() {
-    let languages = [];
-    let languagesPath = path.join(require.resolve('highlight.js'), '../languages')
-    fs.readdirSync(languagesPath).map(function(file) {
-      let lang = require(path.join(languagesPath, file))(hljs)
+		languages = reorderForCompatibility(languages)
 
-      let definition = {
-        aliases: lang.aliases || [],
-        variable: normalizeLanguageName(file.split('.')[0]),
-        name: file.split('.')[0],
-        file: file,
-      }
+		return languages
+	},
 
-      languages.push(definition)
-    })
+	treeForStyles() {
+		let srcPath = path.join(require.resolve('highlight.js'), '..', '..', 'styles')
 
-    languages = reorderForCompatibility(languages);
+		try {
+			fs.statSync(path.join(srcPath, this.style)).isFile()
+		} catch (err) {
+			this.ui.writeWarnLine('[ember-code-block] style does not exist', this.style)
+			// return null;
+		}
 
-    return languages
-  },
+		let tree = new Funnel(srcPath, {
+			files: [this.style + '.css'],
+			annotation: 'Funnel: highlight.js style',
+		})
 
-  _languageTree(language) {
-    let srcPath = path.join(require.resolve('highlight.js'), '..', 'languages');
+		return rename(tree, function() {
+			return '/highlight/styles.css'
+		})
+	},
 
-    let tree = new Funnel(srcPath, {
-      include: [language.file],
-      destDir: language.file,
-      annotation: `Funnel: highlight.js language: ${language.name}`
-    });
+	_languageTree(language) {
+		let srcPath = path.join(require.resolve('highlight.js'), '..', 'languages')
 
-    let srcTree = new AMDDefineFilter(tree, `highlight/languages/${language.name}`, {
-      rewriterFunction: rewriteLanguageDefinition
-    });
+		let tree = new Funnel(srcPath, {
+			include: [language.file],
+			destDir: language.file,
+			annotation: `Funnel: highlight.js language: ${language.name}`,
+		})
 
-    return rename(srcTree, function() {
-      return `/highlight/${language.file}`;
-    });
-  },
+		let srcTree = new AMDDefineFilter(tree, `highlight/languages/${language.name}`, {
+			rewriterFunction: rewriteLanguageDefinition,
+		})
 
-  _highlightTree() {
-    // Package up the highlight.js source from its node module.
-    let srcPath = path.join(require.resolve('highlight.js'), '..');
+		return rename(srcTree, function() {
+			return `/highlight/${language.file}`
+		})
+	},
 
-    let tree = new Funnel(srcPath, {
-      include: ['highlight.js'],
-      destDir: `/highlight.js`,
-      annotation: `Funnel: highlight.js`
-    });
+	_highlightTree() {
+		// Package up the highlight.js source from its node module.
+		let srcPath = path.join(require.resolve('highlight.js'), '..')
 
-    // console.log(this.languages);
-    let srcTree = new AMDDefineFilter(tree, "highlight", {languages: this.languages});
-    return rename(srcTree, function() {
-      return `/highlight/highlight.js`;
-    });
-  }
+		let tree = new Funnel(srcPath, {
+			files: ['highlight.js'],
+			destDir: `highlight`,
+			annotation: `Funnel: highlight.js`,
+		})
 
-};
+		return tree
+
+		// console.log(this.languages);
+		// let srcTree = new AMDDefineFilter(tree, 'highlight', {
+		// 	languages: this.languages,
+		// })
+		// return rename(srcTree, function() {
+		// 	return `/highlight/highlight.js`
+		// })
+	},
+}
+
+function findTargetHost(addon, app) {
+	let target = app
+
+	if (typeof addon.import === 'function') {
+		target = addon
+	} else {
+		// If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+		// use that.
+		if (typeof addon._findHost === 'function') {
+			target = addon._findHost()
+		}
+
+		// Otherwise, we'll use this implementation borrowed from the _findHost()
+		// method in ember-cli.
+		// Keep iterating upward until we don't have a grandparent.
+		// Has to do this grandparent check because at some point we hit the project.
+		let current = addon
+		do {
+			target = current.app || app
+		} while (current.parent.parent && (current = current.parent))
+	}
+
+	return target
+}
